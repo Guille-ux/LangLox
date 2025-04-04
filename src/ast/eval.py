f40e42bc833d7f35a6357496cf872952f317bbd0
Guille <guilleleiratemes@gmail.com>
2025-04-04 16:57:24 +0100

    ZynkPy is now a calculator!

---
+++ src/ast/eval.py
@@ -0,0 +1,90 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# 
+# Copyright (c) 2025 Guillermo Leira Temes
+
+#Visitor pattern (it is very helpful)
+
+from . import expressions as zexpr
+
+class Visitor:
+	def visit_literal(self, expr):
+		raise NotImplementedError()
+	def visit_binary(self, expr):
+		raise NotImplementedError()
+	def visit_unary(self, expr):
+		raise NotImplementedError()
+	def visit_grouping(self, expr):
+		raise NotImplementedError()
+
+class ZynkEval(Visitor):
+	def visit(self, expr):
+		if isinstance(expr, zexpr.Literal):
+			return self.visit_literal(expr)
+		elif isinstance(expr, zexpr.Binary):
+			return self.visit_binary(expr)
+		elif isinstance(expr, zexpr.Unary):
+			return self.visit_unary(expr)
+		elif isinstance(expr, zexpr.Grouping):
+			return self.visit_grouping(expr)
+		else:
+			raise ValueError(f"¡Unknow Expression type : {type(expr)}")
+	def visit_literal(self, expr):
+		return expr.value
+	def visit_binary(self, expr):
+		left = expr.left.accept(self)
+		right = expr.right.accept(self)
+		try:
+			#lógica
+			if expr.operator == "+":
+				return left + right
+			elif expr.operator == "and":
+				return left and right
+			elif expr.operator == "or":
+				return left or right
+			elif expr.operator == "^":
+				return left ^ right
+			elif expr.operator == "<":
+				return left < right
+			elif expr.operator == "==":
+				return left == right
+			elif expr.operator == "!=":
+				return left != right
+			elif expr.operator == "<=":
+				return left <= right
+			elif expr.operator == ">":
+				return left > right
+			elif expr.operator == ">=":
+				return left >= right
+			elif expr.operator == "-":
+				return left - right
+			elif expr.operator == "*":
+				return left * right
+			elif expr.operator == "/":
+				return left / right
+			else:
+				raise ValueError(f"¡Operator : {expr.operator} isn't recognized!")
+		except Exception as e:
+			raise Exception(f"¡Invalid Calc : {expr.left} {expr.operator} {expr.right} !")
+	def visit_unary(self, expr):
+		sign = expr.right.accept(self)
+		
+		#logic
+		if expr.operator == "-":
+			return -sign
+		elif expr.operator == "!":
+			return not sign
+		else:
+			raise ValueError(f"¡Operator : {expr.operator} isn't recognized!")
+	def visit_grouping(self, expr):
+		return expr.expr.accept(self)
---
+++ src/tokens.py
@@ -0,0 +1,98 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# 
+# Copyright (c) 2025 Guillermo Leira Temes
+
+from enum import Enum
+
+class TokenType(Enum):
+	# Literales
+	INT = "INT"
+	FLOAT = "FLOAT"
+	NULL = "NULL"
+	STRING = "STRING"
+	BOOL = "BOOL"
+	
+	#IDENTIFICADORES
+	IDENTIFIER = "IDENTIFIER"
+	
+	#KEYWORDS
+	CLASS = "CLASS"
+	ELSE = "ELSE"
+	FALSE = "FALSE"
+	FUNC = "FUNC"
+	FOR = "FOR"
+	IF = "IF"
+	RETURN = "RETURN"
+	SUPER = "SUPER"
+	THIS = "THIS"
+	TRUE = "TRUE"
+	VAR = "VAR"
+	WHILE = "WHILE"
+	CALL = "CALL"
+	NEW = "NEW"
+	
+	#operands
+	PLUS = "+"
+	MINUS = "-"
+	MULTIPLY = "*"
+	DIVIDE = "/"
+	EQUALS = "=="
+	ASSIGN = "="
+	GREATER = ">"
+	GREATER_EQUAL = ">="
+	LESS_EQUAL = "<="
+	LESS = "<"
+	OR = "or"
+	NOT = "!"
+	XOR = "^"
+	AND = "and"
+	NOT_EQUAL = "!="
+	
+	
+	#delimitadores
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+	COMMA = ","
+	SEMICOLON = ";"
+	
+	#Otros tipos
+	EOF = "EOF"
+	DOT = "."
+
+	#built-in
+	PRINT = "PRINT"
+	IMPORT = "IMPORT"
+	
+class Token:
+	def __init__(self, tipo, lexem, valor, line, column):
+		self.line = line
+		self.column = column
+		self.type = tipo
+		self.value = valor
+		self.included = []
+		self.lexem = lexem
+	def include(self, token):
+		self.included.append(token)
+	def __repr__(self):
+		return f"Token(type={self.type}, lexem={self.lexem}, value={self.value})"
+	def __str__(self):
+		return f"Token of the type {self.type} with the value {self.value} with the lexem {self.lexem}"
+	def __eq__(self, other):
+		return self.type == other.type and self.value == other.value
+	def get_pos(self):
+		return (self.line, self.column)
+
+
