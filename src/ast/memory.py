604c6011ee59ef283a79d66a4207f557c1a1ddaa
Guille-ux <guillermoleiratemes@protonmail.com>
2025-04-11 02:58:40 +0200

    more funcs to memory

--- src/ast/memory.py
+++ src/ast/memory.py
@@ -48,7 +48,7 @@ class Memory:
         self.modules = {}
         self.enclosing = enclosing
     def add_function(self, name, func):
-        self.func_memory[name] = func
+        self.func_memory.add(name, func)
     def add_class(self, name, class_):
         self.classes[name] = class_
     def add_module(self, name, module):
@@ -69,3 +69,17 @@ class Memory:
         raise ValueError(f"Module '{name}' not defined.")
     def get_variable(self, name):
         self.var_memory.get(name)
+    def add_variable(self, name, value):
+        self.var_memory.add(name, value)
+    def remove_variable(self, name):
+        self.var_memory.remove(name)
+    def __str__(self):
+        return str(self.var_memory) + "\n" + str(self.func_memory) + "\n" + str(self.classes) + "\n" + str(self.modules)
+    def __repr__(self):
+        return str(self.var_memory) + "\n" + str(self.func_memory) + "\n" + str(self.classes) + "\n" + str(self.modules)
+    def remove_function(self, name):
+        self.func_memory.remove(name)
+    def remove_class(self, name):
+        del self.classes[name]
+    def remove_module(self, name):
+        del self.classes[name]
